f723a762f747807675a34e1297c4384268d48130
jim0625 <71261463+jim0625@users.noreply.github.com>
2023-05-24 18:26:09 +0800

    fix (#8)

--- components/Task.js
+++ components/Task.js
@@ -6,7 +6,7 @@ const Task = ({ taskText, onClick }) => {
       <div className=" mb-[10px] flex w-[70%] flex-1 rounded-[15px] bg-[#031956] text-[#b6c7db]">
         <div className="flex w-full items-center justify-between p-[20px] text-xl">{taskText}</div>
       </div>
-      <BsFillTrashFill onClick={onClick} className="ml-10 cursor-pointer text-2xl" />
+       <BsFillTrashFill onClick={onClick} className="ml-10 cursor-pointer text-2xl" />
     </div>
   );
 };
